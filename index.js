b7078f81e63a4006c3c4f7e1a95789189ab5ec5b
Ziddah Edem <ziddah.edem@gmail.com>
2020-11-18 06:27:48 +0000

    :tada: initial commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const signUp = document.getElementById("sign-up");
 setTimeout(() => {
   container.classList.add("sign-in");
 }, 200);
+
 const toggle = () => {
   container.classList.toggle("sign-in");
   container.classList.toggle("sign-up");
